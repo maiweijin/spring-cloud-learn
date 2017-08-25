f99018994b65ed389f1ea637c1b35e97d82c8143
MWJ <maiweijin@fssddlbole.com>
2017-08-25 17:00:13 +0800

    feign客户端同时使用jaxrs和spring mvc注解

--- feign-client/src/main/java/com/bonade/cloud/client/health/ServiceHealthCheck.java
+++ feign-client/src/main/java/com/bonade/cloud/client/health/ServiceHealthCheck.java
@@ -8,12 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.bonade.cloud.client.inter.FeignClientsConfigurationExtend;
-
-@FeignClient(value = "ADD-SERVICE", configuration = FeignClientsConfigurationExtend.class)
 public interface ServiceHealthCheck {
 
 	default Health health() {
@@ -28,6 +23,5 @@ public interface ServiceHealthCheck {
 	@GET
 	@Path("health")
 	@Produces(MediaType.APPLICATION_JSON)
-	@GetMapping(value = "health", consumes = MediaType.APPLICATION_JSON)
 	Map<String, Object> health0();
 }
--- feign-client/src/main/java/com/bonade/cloud/client/inter/ServiceConsumer.java
+++ feign-client/src/main/java/com/bonade/cloud/client/inter/ServiceConsumer.java
@@ -1,10 +1,10 @@
 package com.bonade.cloud.client.inter;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.cloud.netflix.feign.support.SpringMvcContract;
 
 import com.bonade.cloud.service.TestService;
 
-@FeignClient(value = "add-service", fallback = ServiceConsumerHystrix.class, configuration = FeignClientsConfiguration.class)
+@FeignClient(value = "add-service", fallback = ServiceConsumerHystrix.class, configuration = SpringMvcContract.class)
 public interface ServiceConsumer extends TestService {
 }
