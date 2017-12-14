a0d0cee577f0faa8f907e272c762217fb975f0ad
MWJ <maiweijin@fssddlbole.com>
2017-12-14 16:43:17 +0800

    add oauth jdbcimpl

--- oauth-demo/src/main/java/com/bonade/cloud/oauthdemo/JdbcOauthConfig.java
+++ oauth-demo/src/main/java/com/bonade/cloud/oauthdemo/JdbcOauthConfig.java
@@ -1,4 +1,54 @@
 package com.bonade.cloud.oauthdemo;
 
-public class JdbcOauthConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.builders.ClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.builders.JdbcClientDetailsServiceBuilder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
+@Configuration
+public class JdbcOauthConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private DataSource dataSource;
+    @Bean // 声明TokenStore实现
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+    @Bean // 声明 ClientDetails实现
+    public ClientDetailsService clientDetails() {
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    @Override // 配置框架应用上述实现
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore());
+        // 配置TokenServices参数
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(endpoints.getTokenStore());
+        tokenServices.setSupportRefreshToken(false);
+        tokenServices.setClientDetailsService(endpoints.getClientDetailsService());
+        tokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
+        tokenServices.setAccessTokenValiditySeconds( (int) TimeUnit.DAYS.toSeconds(30)); // 30天
+
+        endpoints.tokenServices(tokenServices);
+    }
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
 }
--- oauth-demo/src/main/java/com/bonade/cloud/oauthdemo/OauthDemoApplication.java
+++ oauth-demo/src/main/java/com/bonade/cloud/oauthdemo/OauthDemoApplication.java
@@ -3,6 +3,8 @@ package com.bonade.cloud.oauthdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.oauth2.config.annotation.builders.ClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
@@ -11,6 +13,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class OauthDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthDemoApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(OauthDemoApplication.class, args);
 	}
 }
